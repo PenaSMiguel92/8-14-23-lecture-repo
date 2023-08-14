cd78a1fe2dbbec27acdfe563a24c455b5fddb28c
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-14 14:29:44 -0700

    covering abstract classes, model

---
+++ CarnivourousPlant.java
@@ -0,0 +1,11 @@
+abstract class CarnivourousPlant {
+    private String name;
+    private boolean isHungry;
+
+    public CarnivourousPlant(String name) {
+        this.name = name;
+        this.isHungry = true;
+    }
+
+    
+}
