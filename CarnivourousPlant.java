278346302ef53d820686ae595fc6ac6d18f1bd50
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-14 14:35:22 -0700

    covering setters and getters

--- CarnivourousPlant.java
+++ CarnivourousPlant.java
@@ -7,5 +7,16 @@ abstract class CarnivourousPlant {
         this.isHungry = true;
     }
 
-    
+    public String getName() {
+        return this.name;
+    }
+
+    public boolean getIsHungry() {
+        return this.isHungry;
+    }
+
+    public void setIsHungry(boolean hungry) {
+        this.isHungry = hungry;
+    }
+
 }
